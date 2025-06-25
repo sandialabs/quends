3ae209668da0b51adcecafdd84a071f2f14fa555
Abeyah Calpatura <adcalpa@sandia.gov>
2025-06-25 10:03:11 -0700

    update tutorials

---
+++ examples/tutorial/datastream_guide.py
@@ -0,0 +1,199 @@
+r"""
+DataStream Class
+----------------
+This tutorial demonstrates the usage of the DataStream class,
+which provides methods for analyzing time-series data.
+
+The following features are:
+    - **Trimming**: Identifies steady-state regions in data.
+    - **Statistical Analysis**: Computes mean, standard deviation, confidence intervals, and cumulative statistics.
+    - **Stationarity Testing**: Uses the Augmented Dickey-Fuller test.
+    - **Effective Sample Size (ESS)**: Estimates the independent sample size.
+    - **Optimal Window Size**: Determines the best window for data smoothing.
+"""
+
+# %%
+# Import DataStream
+import quends as qnds
+
+# %%
+# GX Data Analysis
+# ----------------
+# Analysis on GX Data
+
+# Specify the file paths
+csv_file_path = "gx/tprim_2_0.out.csv"
+csv2_file_path = "gx/ensemble/tprim_2_5_a.out.csv"
+
+# Load the data from CSV files
+data_stream_csv = qnds.from_csv(csv_file_path)
+data_stream_gx = qnds.from_csv(csv2_file_path)
+
+# Display the first few rows of the GX data
+data_stream_gx.head()
+
+# %%
+# Get available variables
+data_stream_gx.variables()
+
+# %%
+# Get number of rows from the following data in GX
+len(data_stream_gx)
+
+# %%
+# Stationary Check
+# ~~~~~~~~~~~~~~~~
+#
+
+# Check if a single column is stationary
+data_stream_gx.is_stationary("HeatFlux_st")
+
+# Check if multiple columns are stationary
+data_stream_gx.is_stationary(["HeatFlux_st", "Wg_st", "Phi2_t"])
+
+# %%
+# Trimming data based to obtain steady-state portion
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#
+
+# Trim the data based on standard deviation method
+trimmed_df = data_stream_gx.trim(
+    column_name="HeatFlux_st", window_size=50, method="std"
+)
+# View trimmed data
+trimmed_df.head()
+
+# %%
+new_trimmed_df = trimmed_df.trim(
+    column_name="HeatFlux_st", window_size=50, method="std"
+)
+new_trimmed_df.head()
+
+# %%
+# Trim the data based on threshold method
+trimmed_df = data_stream_gx.trim(
+    column_name="HeatFlux_st", window_size=50, method="threshold", threshold=0.1
+)
+# View trimmed data
+trimmed_df.head()
+
+# %%
+# Trim the data based on rolling variance method
+trimmed_df = data_stream_gx.trim(
+    column_name="HeatFlux_st", window_size=50, method="rolling_variance", threshold=0.10
+)
+# View trimmed data
+trimmed_df.head()
+
+# %%
+# Effective Sample Size
+# ~~~~~~~~~~~~~~~~~~~~~
+#
+# Compute Effective Sample Size for specific columns in GX
+ess_dict = data_stream_gx.effective_sample_size(column_names=["HeatFlux_st", "Wg_st"])
+print(ess_dict)
+
+# %%
+# Compute Effective sample size for trimmed data
+ess_df = trimmed_df.effective_sample_size()
+print(ess_df)
+
+# %%
+# UQ Analysis
+# -----------
+#
+# Compute Statistics on trimmed dataframe
+
+stats = trimmed_df.compute_statistics(method="sliding")
+print(stats)
+
+# %%
+# Exporter
+# Below Displays the information as a DataFrame
+exporter = qnds.Exporter()
+exporter.display_dataframe(stats)
+
+# %%
+# Below Displays the information in JSON
+
+exporter.display_json(stats)
+
+# %%
+# Other statistical methods
+# ~~~~~~~~~~~~~~~~~~~~~~~~~
+
+# %%
+# Calculate the mean with a window size of 10
+mean_df = trimmed_df.mean(window_size=10)
+print(mean_df)
+
+# %%
+# Calculate the mean with the method of sliding
+mean_df = trimmed_df.mean(method="sliding")
+print(mean_df)
+
+# %%
+# Calculate the mean uncertainty
+uq_df = trimmed_df.mean_uncertainty()
+print(uq_df)
+
+# %%
+# Calculate the mean uncertainty with the method of sliding
+uq_df = trimmed_df.mean_uncertainty(method="sliding")
+uq_df
+
+# %%
+# Calculate the confidence intervale with the trimmed dataframe
+ci_df = trimmed_df.confidence_interval()
+print(ci_df)
+
+# %%
+# Optimal Window
+# ~~~~~~~~~~~~~~
+#
+
+# %%
+# Calulcautes the optimal window size
+optimal_df = trimmed_df.optimal_window_size()
+print(optimal_df)
+
+# %%
+# Cumlative Statistics
+cumulative_df = trimmed_df.cumulative_statistics()
+print(cumulative_df)
+
+# %%
+# Display Cumulative Statistics as a DataFrame
+exporter.display_dataframe(cumulative_df)
+
+# %%
+# CGYRO Data Analysis
+# ~~~~~~~~~~~~~~~~~~~
+#
+
+# %%
+# Specify the file paths
+csv_file_path = "cgyro/output_nu0_50.csv"
+data_stream_cg = qnds.from_csv(csv_file_path)
+data_stream_cg.head()
+
+# %%
+# Get the number of rows
+len(data_stream_cg)
+
+# Trim the data based on threshold method
+trimmed_df = data_stream_cg.trim(
+    column_name="Q_D/Q_GBD", window_size=10, method="std", robust=True
+)
+# View trimmed data
+trimmed_df.head()
+
+# %%
+# Compute Statistics with the method "sliding"
+stats = trimmed_df.compute_statistics(method="sliding")
+print(stats)
+
+# %%
+# Compute Statistics
+stats = trimmed_df.compute_statistics()
+print(stats)
