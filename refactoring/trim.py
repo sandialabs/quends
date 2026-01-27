52decb05e6144f2d809cb8f0d02a501002802854
adcalpa <adcalpa@sandia.gov>
2026-01-27 10:33:53 -0800

    refactor files for data_stream.py

--- refactoring/trim.py
+++ refactoring/trim.py
@@ -4,32 +4,6 @@ from typing import Any
 from .data_stream import DataStream
 from .operations import DataStreamOperation
 
-"""
-Usage signature for the new trimming workflow::
-
-    strategy = SomeTrimStrategy(kwargs...)
-    trim_operation = TrimDataStreamOperation(strategy=strategy)
-    data_stream = DataStream(data)
-    trimmed_data_stream = trim_operation(data_stream)
-"""
-
-# Goes into datastream
-# class DataStream:
-
-#     def __init__(self, data: Any, history: Optional[DataStreamHistory] = None) -> None:
-#         self._data = data
-#         self._history = history or DataStreamHistory()
-
-#     @property
-#     def data(self) -> Any:
-#         return self._data
-
-#     @property
-#     def history(self) -> DataStreamHistory:
-#         return self._history
-
-# Now let's describe different trim strategies
-
 
 # Abstract base class for a trim strategy
 # This is the class that a potential third-party user could inherit
